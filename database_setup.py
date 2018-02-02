5a68367979bcc02bb9911dedea7d4d847c7f0e43
Nahuelito <nahuel.albino@gmail.com>
2018-02-02 11:23:44 -0300

    Change database engine

--- database_setup.py
+++ database_setup.py
@@ -64,6 +64,6 @@ class Item(Base):
         }
 
 
-engine = create_engine('sqlite:///itemcatalog.db')
+engine = create_engine('postgresql://catalog:password@localhost/itemcatalog')
 
 Base.metadata.create_all(engine)
